3db9aa3e2e665757abd96697e05a173c33188e9f
nouranelsakiely <nourk632@gmail.com>
2019-12-31 14:37:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="../img/icons/favicon.ico"/>
-	<link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="../vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="../vendor/daterangepicker/daterangepicker.css">
-	<link rel="stylesheet" type="text/css" href="../css/util.css">
-	<link rel="stylesheet" type="text/css" href="../css/main2.css">
+	<link rel="icon" type="image/png" href="https://github.com/nouranelsakiely/heroku/tree/master/img/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/css/util.css">
+	<link rel="stylesheet" type="text/css" href="https://github.com/nouranelsakiely/heroku/tree/master/css/main2.css">
 </head>
 <body style="background-color: #666666;">
 	
@@ -56,15 +56,15 @@
 	
 
 
-	<script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
-	<script src="../vendor/animsition/js/animsition.min.js"></script>
-	<script src="../vendor/bootstrap/js/popper.js"></script>
-	<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="../vendor/select2/select2.min.js"></script>
-	<script src="../vendor/daterangepicker/moment.min.js"></script>
-	<script src="../vendor/daterangepicker/daterangepicker.js"></script>
-	<script src="../vendor/countdowntime/countdowntime.js"></script>
-	<script src="../js/main.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/animsition/js/animsition.min.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/bootstrap/js/popper.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/select2/select2.min.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/daterangepicker/moment.min.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/vendor/countdowntime/countdowntime.js"></script>
+	<script src="https://github.com/nouranelsakiely/heroku/tree/master/js/main.js"></script>
 
 </body>
 </html>
